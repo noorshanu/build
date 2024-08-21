9e527293dc6abf45a1c9ea5392cec83ffb792ebb
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-08-21 16:28:10 +0530

    asas

--- src/index.js
+++ src/index.js
@@ -18,9 +18,35 @@ app.use(cookieParser());
 app.use(express.json());
 
 // CORS setup for Express
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000', // Frontend URL
+//     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true,
+//     exposedHeaders: [
+//       'Content-Disposition',
+//       'X-Auth-Token',
+//       'Authorization',
+//       'Set-Cookie',
+//     ],
+//   }),
+// );
+//index me ye change krna hoga
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://freelance.deelance.com',
+]; // Add more allowed origins if necessary
+
 app.use(
   cors({
-    origin: 'https://freelance.deelance.com', // Frontend URL
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -32,7 +58,6 @@ app.use(
     ],
   }),
 );
-
 // Serve static files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
@@ -43,7 +68,7 @@ app.get('/', (req, res) => {
 // Socket.io setup with CORS
 const io = new Server(server, {
   cors: {
-    origin: 'https://freelance.deelance.com', //====Your frontend URL=====//
+    origin: 'http://localhost:3000', //====Your frontend URL=====//
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -65,35 +90,35 @@ io.on('connection', socket => {
 });
 
 //==================production url======================//
-mongoose
-  .connect(process.env.MONGODB_URI_second, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000,
-  })
-  .then(() => {
-    console.log(
-      'MongoDB connected successfully',
-      process.env.MONGODB_URI_second,
-    );
-  })
-  .catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-  });
-
-//======================platform url=====================//
 // mongoose
-//   .connect(process.env.MONGODB_URI_PRIMARY, {
+//   .connect(process.env.MONGODB_URI_second, {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
 //     serverSelectionTimeoutMS: 30000,
 //   })
 //   .then(() => {
-//     console.log('MongoDB connected successfully');
+//     console.log(
+//       'MongoDB connected successfully',
+//       process.env.MONGODB_URI_second,
+//     );
 //   })
 //   .catch(err => {
 //     console.error('Error connecting to MongoDB:', err);
 //   });
+
+//======================platform url=====================//
+mongoose
+  .connect(process.env.MONGODB_URI_PRIMARY, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000,
+  })
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 // Set io instance to app to be accessed in routes
 app.set('io', io);
 app.use('/', route);
